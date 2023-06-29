bf3d5a87810f6c94244d6b35344e12910b18c07d
Nico Rithner <nicorithner@gmail.com>
2023-06-29 16:12:26 -0600

    Tuple type

--- using-types/app.ts
+++ using-types/app.ts
@@ -16,9 +16,22 @@
 
 // console.log(person.name);
 
-// Array types
-const person = {
+// // Array types
+// const person = {
+//   name: "maximilian",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+// };
+
+// Tuple type
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string]; // this enforces length and types.
+} = {
   name: "maximilian",
   age: 30,
   hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
 };
