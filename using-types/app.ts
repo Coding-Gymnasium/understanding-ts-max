8d919603853906f3f4fd8813432acaa90a82626b
Nico Rithner <nicorithner@gmail.com>
2023-06-29 16:07:59 -0600

    Array type

--- using-types/app.ts
+++ using-types/app.ts
@@ -5,13 +5,20 @@
 
 // console.log(person.nickname); // this causes an error because property 'nickname' doesn't exist in this object.
 
-// explicitely assign types
-const person: {
-  name: string;
-  age: number;
-} = {
+// // explicitely assign types
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+//   name: "maximilian",
+//   age: 30,
+// };
+
+// console.log(person.name);
+
+// Array types
+const person = {
   name: "maximilian",
   age: 30,
+  hobbies: ["Sports", "Cooking"],
 };
-
-console.log(person.name); // this causes an error because property 'nickname' doesn't exist in this object.
