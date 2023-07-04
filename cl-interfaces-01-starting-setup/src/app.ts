cc49e8bded1743aa37b273358b3378a0ddf07313
Nico Rithner <nicorithner@gmail.com>
2023-07-04 13:40:07 -0600

    Complete access modifiers lesson

---
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -0,0 +1,29 @@
+class Department {
+  name: string;
+  private employees: string[] = [];
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+const sales = new Department("Sales");
+console.log(sales);
+console.log(sales.describe());
+
+sales.addEmployee('Max');
+sales.addEmployee('Manu');
+sales.printEmployeeInformation();
