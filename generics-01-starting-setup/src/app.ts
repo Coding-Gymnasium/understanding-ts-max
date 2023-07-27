0e1c8bd1a713e0e391639fc16988d8cd0317fc97
Nico Rithner <nicorithner@gmail.com>
2023-07-27 08:04:32 -0600

    Creating a Generic function lesson

--- generics-01-starting-setup/src/app.ts
+++ generics-01-starting-setup/src/app.ts
@@ -1,10 +1,28 @@
-// const names = ['Max', 'Manuel']; // type string[]
-const arr = []; // type any
-const names: Array<string> = []; // type string[]
-// names[0].split(' ');
+// // const names = ['Max', 'Manuel']; // type string[]
+// const arr = []; // type any
+// const names: Array<string> = []; // type string[]
+// // names[0].split(' ');
 
-const promise: Promise<string> = new Promise((resolve, _reject) => {
-  setTimeout(() => {
-    resolve("This is done");
-  }, 2000);
-});
+// const promise: Promise<string> = new Promise((resolve, _reject) => {
+//   setTimeout(() => {
+//     resolve("This is done");
+//   }, 2000);
+// });
+
+function merge(objA: object, objB: object) {
+  return Object.assign(objA, objB);
+}
+
+// using type casting
+const mergedObj = merge({ name: "Max" }, { age: 30 }) as {
+  name: string;
+  age: number;
+};
+
+// Generic types
+
+function mergeGen<T, U>(objA: T, objB: U) {
+  return Object.assign({}, objA, objB);
+}
+const mergedObj2 = mergeGen({ name: "Max" }, { age: 30 });
+console.log(mergedObj2.age)
