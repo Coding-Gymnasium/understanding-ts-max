dc3f1dcdf3f19a40c0231aa41aca643c58c192bd
Nico Rithner <nicorithner@gmail.com>
2023-08-01 12:34:43 -0600

    Complete lesson 107

--- decorators-01-starting-setup/src/app.ts
+++ decorators-01-starting-setup/src/app.ts
@@ -13,10 +13,12 @@ function Logger(logString: string) {
 }
 
 function withTemplate(template: string, hookId: string) {
-  return function (_constructor: Function) {
+  return function (constructor: any) {
     const hookEl = document.getElementById(hookId);
+    const p = new constructor();
     if (hookEl) {
       hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = p.name;
     }
   };
 }
