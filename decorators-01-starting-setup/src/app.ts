2fa7a6dbe67ee661ec93468b4c55dfbe5e7e0d7b
Nico Rithner <nicorithner@gmail.com>
2023-08-06 09:01:07 -0600

    Use decorators to add functionality. Add binding

--- decorators-01-starting-setup/src/app.ts
+++ decorators-01-starting-setup/src/app.ts
@@ -53,16 +53,17 @@ function Log(target: any, propertyName: string | Symbol) {
   console.log(target, propertyName);
 }
 
-function Log2(
-  target: any,
-  name: string,
-  descriptor: PropertyDescriptor
-): PropertyDescriptor {
+// function Log2(
+//   target: any,
+//   name: string,
+//   descriptor: PropertyDescriptor
+// ): PropertyDescriptor {
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
   console.log("Accessor decorator!");
   console.log(target);
   console.log(name);
   console.log(descriptor);
-  return {};
+  // return {};
 }
 
 function Log3(
@@ -110,3 +111,34 @@ class Product {
 
 const p1 = new Product("Book", 19);
 const p2 = new Product("Book 2", 29);
+
+function Autobind(
+  _target: any,
+  _methodName: string | Symbol | number,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+class Printer {
+  message = "This works!";
+
+  @Autobind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer();
+
+const button = document.querySelector("button")!;
+// button.addEventListener("click", p.showMessage.bind(p));
+button.addEventListener("click", p.showMessage);
