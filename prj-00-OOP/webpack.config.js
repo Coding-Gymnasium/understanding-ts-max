cd8bb8917f2457fca2c6091ca2d0d93867e4491c
Nico Rithner <nicorithner@gmail.com>
2023-09-08 07:14:05 -0600

    Adjust webpack.config.js
    Add publicPath and devServer

--- prj-00-OOP/webpack.config.js
+++ prj-00-OOP/webpack.config.js
@@ -1,17 +1,25 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/app.ts",
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname),
+      },
+    ],
+  },
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist/",
   },
-  mode: 'development',
   devtool: "inline-source-map",
   module: {
     rules: [
       {
-        test: /\.ts$/,
+        test: /\.ts?$/,
         use: "ts-loader",
         exclude: /node_modules/,
       },
