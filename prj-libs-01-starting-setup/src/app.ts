58ce787168dc0410e621ae0a0b6ed110090eaac2
Nico Rithner <nicorithner@gmail.com>
2023-12-07 20:41:54 -0700

    Start set up to use google api

--- prj-libs-01-starting-setup/src/app.ts
+++ prj-libs-01-starting-setup/src/app.ts
@@ -1,12 +1,14 @@
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
 
+// const googleApiKey = process.env.REACT_APP_GOOGLE_API_KEY;
+
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
+  console.log(enteredAddress);
 
   // send this to Google's API
 }
 
-form.addEventListener("submit", () => {});
-
+form.addEventListener("submit", searchAddressHandler);
