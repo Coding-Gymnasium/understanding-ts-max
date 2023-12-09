f4f61ce6571bd144b1337adad0ba2f57d109467c
Nico Rithner <nicorithner@gmail.com>
2023-12-09 16:29:52 -0700

    Start state management

--- react-ts-practice/src/App.tsx
+++ react-ts-practice/src/App.tsx
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import NewTodo from "./components/NewTodo";
 import TodoList from "./components/TodoList";
+import { Todo } from "./todo.models";
 
 const App: React.FC = () => {
-  const todos = [{ id: "t1", text: "Finish the course" }];
+  const [todos, setTodos] = useState<Todo[]>([]);
   const todoAddHandler = (text: string) => {
-    console.log(text);
+    setTodos([{ id: Math.random().toString(), text: text }]);
   };
 
   return (
