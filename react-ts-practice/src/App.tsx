0a2c7ce28a31bdd84d64abf38a64e6199b5df758
Nico Rithner <nicorithner@gmail.com>
2023-12-09 13:20:45 -0700

    Add todos data and pass it as prop

--- react-ts-practice/src/App.tsx
+++ react-ts-practice/src/App.tsx
@@ -1,10 +1,12 @@
 import TodoList from "./components/TodoList";
 
 const App: React.FC = () => {
+  const todos = [{ id: "t1", text: "Finish the course" }];
+
   return (
     <div className="App">
       <h1>ToDo List</h1>
-      <TodoList />
+      <TodoList items={todos} />
     </div>
   );
 };
