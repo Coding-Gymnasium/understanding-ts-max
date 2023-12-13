77ad43954b3eed2f0e1168964900af0b7e9fa762
Nico Rithner <nicorithner@gmail.com>
2023-12-13 07:44:33 -0700

    Add middleware and types

--- node-project-1/src/app.ts
+++ node-project-1/src/app.ts
@@ -1,8 +1,15 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import todoRoutes from "./routes/todos";
 
 const app = express();
 
 //port
 const port = 3000;
+
+app.use("/todos", todoRoutes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
 
 app.listen(port, (): void => console.log(`running on port ${port}`));
